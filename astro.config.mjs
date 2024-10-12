3a1e66440f6d43034e2bb3e5fe6d57c24694e22d
shunya-fug <shunnya0715@gmail.com>
2024-10-13 00:59:08 +0900

    🔧：Web Analytics有効化

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,21 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
+import { defineConfig } from 'astro/config'
+import mdx from '@astrojs/mdx'
 
-import sitemap from '@astrojs/sitemap';
+import sitemap from '@astrojs/sitemap'
 
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel/serverless'
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
   integrations: [mdx(), sitemap(), tailwind()],
   output: 'server',
-  adapter: vercel(),
-});
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
+})
